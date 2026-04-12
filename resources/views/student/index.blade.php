ecf58a5998ee410e8c7f453c87c08aeab4babebe
Nabila Hasyim <nabilahasyimm@gmail.com>
2026-04-12 14:25:14 +0800

    Pertemuan 3: Database

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -2,6 +2,12 @@
 
     <x-slot:title>{{ $title }}</x-slot>
 
+    <ul class="list-group">
+        @foreach ($students as $student)
+            <li class="list-group-item">{{ $loop->iteration }} . {{ $student->nim }} -- {{ $student->name }}</li>
+        @endforeach
 
-    <h1 class="fw-bold">Data Student</h1>
+    </ul>
+
+
 </x-app>
